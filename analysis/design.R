decfe937d9d43e9ba3ce163c9596ad6894985323
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-09-21 08:33:12 +0100

    copy files and run create_project.R

---
+++ analysis/design.R
@@ -0,0 +1,150 @@
+# # # # # # # # # # # # # # # # # # # # #
+# This script:
+# creates metadata for aspects of the study design
+# # # # # # # # # # # # # # # # # # # # #
+
+# Preliminaries ----
+
+## Import libraries ----
+library('tidyverse')
+library('here')
+
+## create output directories ----
+fs::dir_create(here("lib", "design"))
+
+
+
+# number of matching rounds to perform
+
+n_matching_rounds <- 2
+
+
+# define key dates ----
+
+study_dates <- lst(
+  over12 = lst(
+   start_date = "2021-09-20", #start of recruitment monday 20 september pfizer licensed for for over 12yo in england
+   end_date = "2021-12-19", # end of recruitment (13 weeks later)
+   followupend_date = "2022-01-02", # end of follow-up
+  ),
+  under12 = lst(
+    start_date = "2022-04-04", #start of recruitment monday 4 april moderna licensed for for under 12yo in england
+    end_date = "2022-07-03", # end of recruitment (13 weeks later)
+    followupend_date = "2022-07-10" # end of follow-up
+  )
+)
+
+extract_increment <- 14
+
+study_dates$over12$control_extract_dates = as.Date(study_dates$over12$start_date) + (0:26)*extract_increment
+study_dates$under12$control_extract_dates = as.Date(study_dates$under12$start_date) + (0:26)*extract_increment
+
+jsonlite::write_json(study_dates, path = here("lib", "design", "study-dates.json"), auto_unbox=TRUE, pretty =TRUE)
+
+study_params <- lst(
+  
+  # over 12 params
+  over12 = lst(
+   minage = 12,
+   maxage= 15,
+  
+   treatment = "pfizerA",
+  ),
+  
+  # under 12 params
+  under12 = lst(
+    minage = 5,
+    maxage= 11,
+    treatment = "pfizerC",
+  )
+  
+)
+
+jsonlite::write_json(study_params, path = here("lib", "design", "study-params.json"), auto_unbox=TRUE, pretty =TRUE)
+
+# define outcomes ----
+
+events_lookup <- tribble(
+  ~event, ~event_var, ~event_descr,
+
+  # other
+  "test", "covid_test_date", "SARS-CoV-2 test",
+  "dereg", "dereg_date", "Deregistration",
+  "primary_care_covid_case", "primary_care_covid_case_date", "Primary care COVID-19",
+  
+  # effectiveness
+  "postest", "postest_date", "Positive SARS-CoV-2 test",
+  "covidemergency", "covidemergency_date", "COVID-19 A&E attendance",
+  "covidemergencyhosp", "covidemergencyhosp_date", "Admission from COVID-19 A&E",
+  "covidadmitted", "covidadmitted_date", "COVID-19 hospitalisation",
+  "noncovidadmitted", "noncovidadmitted_date", "Non-COVID-19 hospitalisation",
+  
+  "covidadmittedproxy1", "covidadmittedproxy1_date", "COVID-19 hospitalisation (A&E proxy)",
+  "covidadmittedproxy2", "covidadmittedproxy2_date", "COVID-19 hospitalisation (A&E proxy v2)",
+  "covidcritcare", "covidcritcare_date", "COVID-19 critical care",
+  "coviddeath", "coviddeath_date", "COVID-19 death",
+  "noncoviddeath", "noncoviddeath_date", "Non-COVID-19 death",
+  "death", "death_date", "Any death",
+
+  # safety
+  "emergencyhosp", "emergencyhosp_date", "Admission from A&E",
+  "admitted", "admitted_unplanned_date", "Unplanned hospitalisation",
+  "emergency", "emergency_date", "A&E attendance",
+)
+
+
+## lookups to convert coded variables to full, descriptive variables ----
+
+recoder <-
+  lst(
+    subgroups = c(
+      `Main` = "all",
+      `Prior SARS-CoV-2 infection` = "prior_covid_infection"
+    ),
+    status = c(
+      `Unmatched`= "unmatched",
+      `Matched` = "matched"
+    ),
+    treated = c(
+      `Unvaccinated` = "0",
+      `Vaccinated` = "1"
+    ),
+    outcome = set_names(events_lookup$event, events_lookup$event_descr),
+    all = c(` ` = "all"),
+    prior_covid_infection = c(
+      `No prior SARS-CoV-2 infection` = "FALSE",
+      `Prior SARS-CoV-2 infection` = "TRUE"
+    ),
+  )
+
+
+## follow-up time ----
+
+# period width
+postbaselinedays <- 14
+
+# where to split follow-up time after recruitment
+postbaselinecuts <- (0:10)*postbaselinedays
+
+# maximum follow-up
+maxfup <- max(postbaselinecuts)
+
+# matching variables ----
+
+# exact variables
+exact_variables <- c(
+  "age_aug21",
+  "region",
+  "sex",
+  "prior_covid_infection",
+  NULL
+)
+
+# caliper variables
+caliper_variables <- c(
+  #age = 1,
+  NULL
+)
+matching_variables <- c(exact_variables, names(caliper_variables))
+
+
---
+++ analysis/study_definition_treated.py
@@ -0,0 +1,136 @@
+from ast import And
+# Import codelists from codelists.py
+import codelists
+
+# import json module
+import json
+
+from cohortextractor import (
+  StudyDefinition,
+  patients,
+  codelist_from_csv,
+  codelist,
+  filter_codes_by_category,
+  combine_codelists,
+  params
+)
+
+from variables_outcome import vaccination_date_X 
+
+cohort = params["cohort"]
+
+
+# import study dates defined in "./analysis/design.R" script
+with open("./lib/design/study-dates.json") as f:
+  study_dates = json.load(f)
+
+# change these in design.R if necessary
+start_date = study_dates[cohort]["start_date"]
+end_date = study_dates[cohort]["end_date"]
+
+
+# import study parameters defined in "./analysis/design.R" script  
+with open("./lib/design/study-params.json") as f:
+  study_params = json.load(f)
+
+minage = study_params[cohort]["minage"]
+maxage = study_params[cohort]["maxage"]
+treatment = study_params[cohort]["treatment"]
+
+
+
+############################################################
+## inclusion variables
+from variables_inclusion import generate_inclusion_variables 
+inclusion_variables = generate_inclusion_variables(index_date="covid_vax_any_1_date")
+############################################################
+## matching variables
+from variables_matching import generate_matching_variables 
+matching_variables = generate_matching_variables(index_date="covid_vax_any_1_date")
+############################################################
+## matching variables
+from variables_outcome import generate_outcome_variables 
+outcome_variables = generate_outcome_variables(index_date="covid_vax_any_1_date")
+############################################################
+
+
+
+# Specify study defeinition
+study = StudyDefinition(
+  
+  # Configure the expectations framework
+  default_expectations={
+    "date": {"earliest": "2020-01-01", "latest": end_date},
+    "rate": "uniform",
+    "incidence": 0.2,
+    "int": {"distribution": "normal", "mean": 1000, "stddev": 100},
+    "float": {"distribution": "normal", "mean": 25, "stddev": 5},
+  },
+  
+  index_date = start_date,
+  
+  # This line defines the study population
+  population=patients.satisfying(
+    f"""
+      registered
+      AND
+      age_aug21 >= {minage}
+      AND
+      age_aug21 <= {maxage}
+      AND
+      (NOT has_died)
+      AND
+      NOT wchild
+      AND 
+      (covid_vax_any_1_date >= start_date)
+      AND
+      (covid_vax_any_1_date <= end_date)
+      AND 
+      (covid_vax_any_1_date = covid_vax_{treatment}_1_date)
+    """,
+  start_date = patients.fixed_value(start_date),
+  end_date = patients.fixed_value(end_date),  
+  ),
+  
+  **vaccination_date_X(
+    name = "covid_vax_any",
+    index_date = "1900-01-01",
+    n = 2,
+    target_disease_matches="SARS-2 CORONAVIRUS"
+  ),
+
+  # pfizer
+  **vaccination_date_X(
+    name = "covid_vax_pfizerA",
+    # use 1900 to capture all possible recorded covid vaccinations, including date errors
+    # any vaccines occurring before national rollout are later excluded
+    index_date = "1900-01-01", 
+    n = 2,
+    product_name_matches="COVID-19 mRNA Vaccine Comirnaty 30micrograms/0.3ml dose conc for susp for inj MDV (Pfizer)"
+  ),
+  
+  # pfizer approved for use in children (5-11)
+  **vaccination_date_X(
+    name = "covid_vax_pfizerC",
+    index_date = "1900-01-01",
+    n = 2,
+    product_name_matches="COVID-19 mRNA Vaccine Comirnaty Children 5-11yrs 10mcg/0.2ml dose conc for disp for inj MDV (Pfizer)"
+  ),
+
+
+  ##############################################################################
+  # inclusion
+  ##############################################################################
+  **inclusion_variables,    
+  
+  ###############################################################################
+  # matching
+  ##############################################################################
+  **matching_variables,      
+  
+    ###############################################################################
+  # outcomes
+  ##############################################################################
+  **outcome_variables,      
+  
+)

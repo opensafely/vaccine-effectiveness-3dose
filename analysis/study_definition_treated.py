9b195294ed77f497831b7ec938d9c04383c4820d
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-09-27 13:39:19 +0100

    Update study_definition_treated.py

--- analysis/study_definition_treated.py
+++ analysis/study_definition_treated.py
@@ -28,10 +28,11 @@ firstaz_date = study_dates["firstaz_date"]
 firstmoderna_date = study_dates["firstmoderna_date"]
 
 ############################################################
-## functions
-from variables_functions import vaccination_date_X
-############################################################
 ## inclusion variables
+from variables_vax import generate_vax_variables 
+vax_variables = generate_vax_variables(index_date="1900-01-01", n=4)
+############################################################
+# vax variables
 from variables_inclusion import generate_inclusion_variables 
 inclusion_variables = generate_inclusion_variables(index_date="covid_vax_disease_3_date")
 ############################################################
@@ -39,13 +40,17 @@ inclusion_variables = generate_inclusion_variables(index_date="covid_vax_disease
 from variables_jcvi import generate_jcvi_variables 
 jcvi_variables = generate_jcvi_variables(index_date="covid_vax_disease_3_date")
 ############################################################
-## matching variables
-from variables_matching import generate_matching_variables 
-matching_variables = generate_matching_variables(index_date="covid_vax_disease_3_date")
+## demographic variables
+from variables_demographic import generate_demographic_variables 
+demographic_variables = generate_demographic_variables(index_date="covid_vax_disease_3_date")
 ############################################################
-## outcome variables
-from variables_outcome import generate_outcome_variables 
-outcome_variables = generate_outcome_variables(index_date="covid_vax_disease_3_date")
+## pre variables
+from variables_pre import generate_pre_variables 
+pre_variables = generate_pre_variables(index_date="covid_vax_disease_3_date")
+############################################################
+## post variables
+from variables_post import generate_post_variables 
+post_variables = generate_post_variables(index_date="covid_vax_disease_3_date")
 ############################################################
 
 # Specify study definition
@@ -79,54 +84,26 @@ study = StudyDefinition(
   #################################################################
   ## Covid vaccine dates
   #################################################################
-  
-  # pfizer
-  **vaccination_date_X(
-    name = "covid_vax_pfizer",
-    # use 1900 to capture all possible recorded covid vaccinations, including date errors
-    # any vaccines occurring before national rollout are later excluded
-    index_date = "1900-01-01", 
-    n = 4,
-    product_name_matches="COVID-19 mRNA Vaccine Comirnaty 30micrograms/0.3ml dose conc for susp for inj MDV (Pfizer)"
-  ),
-  
-  # az
-  **vaccination_date_X(
-    name = "covid_vax_az",
-    index_date = "1900-01-01",
-    n = 4,
-    product_name_matches="COVID-19 Vac AstraZeneca (ChAdOx1 S recomb) 5x10000000000 viral particles/0.5ml dose sol for inj MDV"
-  ),
-  
-  # moderna
-  **vaccination_date_X(
-    name = "covid_vax_moderna",
-    index_date = "1900-01-01",
-    n = 4,
-    product_name_matches="COVID-19 mRNA Vaccine Spikevax (nucleoside modified) 0.1mg/0.5mL dose disp for inj MDV (Moderna)"
-  ),
-  
-  # any covid vaccine
-  **vaccination_date_X(
-    name = "covid_vax_disease",
-    index_date = "1900-01-01",
-    n = 4,
-    target_disease_matches="SARS-2 CORONAVIRUS"
-  ),
-  
+  **vax_variables,
+    
   ###############################################################################
   # jcvi variables
   ##############################################################################
   **jcvi_variables, 
   
   ###############################################################################
-  # matching
+  # demographic variables
   ##############################################################################
-  **matching_variables,      
+  **demographic_variables,   
+
+  ###############################################################################
+  # pre variables
+  ##############################################################################
+  **pre_variables,      
   
   ###############################################################################
-  # outcomes
+  # posts
   ##############################################################################
-  **outcome_variables,      
+  **post_variables,      
   
 )

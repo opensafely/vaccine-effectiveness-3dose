aec2e2001642c5a3911a29a116abafe4d61d602b
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-09-29 15:56:56 +0100

    Update study_definition_controlfinal.py

--- analysis/study_definition_controlfinal.py
+++ analysis/study_definition_controlfinal.py
@@ -1,4 +1,3 @@
-from ast import And
 # Import codelists from codelists.py
 import codelists
 
@@ -15,9 +14,6 @@ from cohortextractor import (
   params
 )
 
-from variables_outcome import vaccination_date_X 
-
-
 cohort = params["cohort"]
 n_matching_rounds = params["n_matching_rounds"]
 
@@ -26,24 +22,13 @@ with open("./lib/design/study-dates.json") as f:
   study_dates = json.load(f)
 
 # change these in design.R if necessary
-start_date = study_dates[cohort]["start_date"]
-end_date = study_dates[cohort]["end_date"]
-
-
-# import study parameters defined in "./analysis/design.R" script  
-with open("./lib/design/study-params.json") as f:
-  study_params = json.load(f)
-
-minage = study_params[cohort]["minage"]
-maxage = study_params[cohort]["maxage"]
-treatment = study_params[cohort]["treatment"]
-
+index_date = study_dates["index_date"]
 
 
 ############################################################
-## outcome variables
-from variables_outcome import generate_outcome_variables 
-outcome_variables = generate_outcome_variables(index_date="trial_date")
+## post variables
+from variables_post import generate_post_variables 
+post_variables = generate_post_variables(index_date="trial_date")
 ############################################################
 
 
@@ -52,14 +37,12 @@ study = StudyDefinition(
   
   # Configure the expectations framework
   default_expectations={
-    "date": {"earliest": "2020-01-01", "latest": end_date},
+    "date": {"earliest": "2020-01-01", "latest": "today"},
     "rate": "uniform",
     "incidence": 0.2,
     "int": {"distribution": "normal", "mean": 1000, "stddev": 100},
     "float": {"distribution": "normal", "mean": 25, "stddev": 5},
   },
-  
-  index_date = start_date,
   
   # This line defines the study population
   population = patients.which_exist_in_file(f_path=f"output/{cohort}/matchround{n_matching_rounds}/actual/cumulative_matchedcontrols.csv.gz"),
@@ -67,17 +50,12 @@ study = StudyDefinition(
   trial_date = patients.with_value_from_file(f_path=f"output/{cohort}/matchround{n_matching_rounds}/actual/cumulative_matchedcontrols.csv.gz", returning="trial_date", returning_type="date", date_format='YYYY-MM-DD'),
   
   match_id = patients.with_value_from_file(f_path=f"output/{cohort}/matchround{n_matching_rounds}/actual/cumulative_matchedcontrols.csv.gz", returning="match_id", returning_type="int"),
-  
-  
-  ###############################################################################
-  # matching
-  ##############################################################################
-  #**matching_variables,
+
   
   ###############################################################################
-  # outcomes
+  # post variables
   ##############################################################################
-  **outcome_variables,
+  **post_variables,
   
   
 )

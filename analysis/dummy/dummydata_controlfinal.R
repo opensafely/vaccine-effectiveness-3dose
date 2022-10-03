0735bbe8c7b9eb15147d2a62316c47d74757f305
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-10-03 10:44:09 +0100

    Update dummydata_controlfinal.R

--- analysis/dummy/dummydata_controlfinal.R
+++ analysis/dummy/dummydata_controlfinal.R
@@ -18,43 +18,68 @@ if(length(args)==0){
 
 fs::dir_create(here("output", cohort,  "dummydata"))
 
+# no indents to make it easier to compare diff
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){ 
 
-if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")){
-  
-  source(here("lib", "functions", "utility.R"))
-  
-  source(here("analysis", "design.R"))
-  
-  dummy_post <- read_feather(here("lib", "dummydata", "dummy_post.feather")) 
-  
-  vars_post <- names(dummy_post)
-  vars_post <- vars_post[str_detect(vars_post, "_day$")]
-  
-  # dummy_control_final
-  
-  # import all datasets of matched controls, including matching variables
-  data_matchedcontrols <- 
-    map_dfr(
-      seq_len(n_matching_rounds), 
-      ~{read_rds(ghere("output", cohort, glue("matchround", .x), "actual", glue("data_successful_matchedcontrols.rds")))},
-      .id="matching_round"
-    ) %>%
-    select(
-      # select variables with_value_from_file
-      patient_id, trial_date, match_id,
-    )
-  
-  data_matchedcontrols %>%
-    left_join(dummy_post , by = "patient_id") %>%
-    mutate(across(all_of(vars_post), ~ trial_date + lubridate::days(.))) %>% # index on trial_date
-    rename_with(~str_replace(., "_day", "_date"), all_of(vars_post)) %>%
-    write_feather(sink = here("output", cohort, "dummydata", "dummy_control_final.feather"))
-  
-  
+source(here("lib", "functions", "utility.R"))
+
+source(here("analysis", "design.R"))
+
+# import all datasets of matched controls, including matching variables
+data_matchedcontrols <- 
+  map_dfr(
+    seq_len(n_matching_rounds), 
+    ~{read_rds(ghere("output", cohort, glue("matchround", .x), "actual", glue("data_successful_matchedcontrols.rds")))},
+    .id="matching_round"
+  ) %>%
+  mutate(
+    trial_day = as.integer(trial_date - study_dates$index_date)
+  ) %>%
+  select(
+    # select variables with_value_from_file
+    patient_id, trial_day, match_id,
+  )
+
+missing <- function(x, rate){
+  missing_index <- seq_len(length(x))[rbinom(length(x), 1, rate)==1]
+  x[missing_index] <- NA
+  x
+}
+
+
+set.seed(10)
+
+dummydata <- data_matchedcontrols %>%
+  mutate(
+    dereg_day = missing(as.integer(runif(n=n(), trial_day, trial_day+120)), 0.99),
+    primary_care_covid_case_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.7),
+    covid_test_day = missing(as.integer(runif(n=n(), trial_day, trial_day+90)), 0.7),
+    postest_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.7),
+    emergency_day = missing(as.integer(runif(n=n(), trial_day, trial_day+200)), 0.8),
+    emergencyhosp_day = missing(as.integer(runif(n=n(), trial_day, trial_day+200)), 0.85),
+    covidemergency_day = missing(as.integer(runif(n=n(), trial_day, trial_day+120)), 0.8),
+    covidemergencyhosp_day = missing(as.integer(runif(n=n(), trial_day, trial_day+200)), 0.85),
+    covidadmitted_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.7),
+    covidcritcare_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.8),
+    admitted_unplanned_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.7),
+    death_day = missing(as.integer(runif(n=n(), trial_day, trial_day+100)), 0.9),
+    coviddeath_day = missing(death_day, 0.7),
+  )
+
+
+dummydata %>%
+  #convert logical to integer as study defs output 0/1 not TRUE/FALSE
+  # mutate(across(where(is.logical), ~ as.integer(.))) %>%
+  #convert integer days to dates since index date and rename vars
+  mutate(across(ends_with("_day"), ~ as.Date(as.character(study_dates$index_date + .)))) %>%
+  rename_with(~str_replace(., "_day", "_date"), ends_with("_day")) %>%
+  write_feather(sink = here("output", cohort, "dummydata", "dummy_control_final.feather"))
+
+
 } else {
-  
-  # save empty output to save space if running on real data
-  tibble() %>%
-    write_feather(sink = here("output", cohort, "dummydata", "dummy_control_final.feather"))
-  
+
+# save empty output to save space if running on real data
+tibble() %>%
+  write_feather(sink = here("output", cohort, "dummydata", "dummy_control_final.feather"))
+
 }

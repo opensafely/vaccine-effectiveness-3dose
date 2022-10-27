60a86f33ef95f04f239dfb3b467f5e33f4093c3d
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-10-27 16:46:20 +0100

    Update table1.R

--- analysis/matching/table1.R
+++ analysis/matching/table1.R
@@ -1,7 +1,6 @@
 # # # # # # # # # # # # # # # # # # # # #
 # Purpose: describe matching results
-# imports matching data
-# reports on matching coverage, matching flowcharts, creates a "table 1", etc
+# creates "table 1"
 # # # # # # # # # # # # # # # # # # # # #
 
 
@@ -14,6 +13,8 @@ library('lubridate')
 library('here')
 library('glue')
 library('arrow')
+library('gt')
+library('gtsummary')
 
 ## import local functions and parameters ---
 
@@ -29,7 +30,7 @@ args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   removeobjects <- FALSE
-  cohort <- "pfizer"
+  cohort <- "mrna"
 } else {
   #FIXME replace with actual eventual action variables
   removeobjects <- TRUE
@@ -48,100 +49,49 @@ fs::dir_create(output_dir)
 
 ## Import data and derive some variables ----
 
-data_matched <- read_rds(ghere("output", cohort, "match", "data_matched.rds")) 
+data_matched <- read_rds(ghere("output", cohort, "match", "data_matched.rds")) %>%
+  mutate(dayssincevax2 = as.integer(trial_date - vax2_date))
 
 data_treatedeligible_matchstatus <- read_rds(here("output", cohort, "match", "data_treatedeligible_matchstatus.rds"))
 
-
-# matching coverage on each day of recruitment period ----
-
-
-# matching coverage for boosted people
-data_coverage <-
-  data_treatedeligible_matchstatus %>%
-  group_by(vax3_date) %>%
-  summarise(
-    n_eligible = n(),
-    n_matched = sum(matched, na.rm=TRUE),
-  ) %>%
-  mutate(
-    n_unmatched = n_eligible - n_matched,
-  ) %>%
-  pivot_longer(
-    cols = c(n_unmatched, n_matched),
-    names_to = "status",
-    names_prefix = "n_",
-    values_to = "n"
-  ) %>%
-  arrange(vax3_date, status) %>%
-  group_by(vax3_date, status) %>%
-  summarise(
-    n = sum(n),
-  ) %>%
-  group_by(status) %>%
-  complete(
-    vax3_date = full_seq(c(dates$start_date, dates$end_date), 1), # go X days before to
-    fill = list(n=0)
-  ) %>%
-  mutate(
-    cumuln = cumsum(n)
-  ) %>%
-  ungroup() %>%
-  mutate(
-    status = factor(status, levels=c("unmatched", "matched")),
-    status_descr = fct_recoderelevel(status, recoder$status)
-  ) %>%
-  arrange(status_descr, vax3_date)
-
-# plot coverage for checking (not release)
-plot_coverage <- data_coverage %>%
-  pivot_longer(cols = c(n, cumuln)) %>%
-  mutate(across(name, factor, levels = c("n", "cumuln"))) %>%
-  ggplot(aes(x=vax3_date, y=value, fill=status_descr)) +
-  geom_bar(stat="identity") +
-  facet_grid(name~., scales = "free_y") +
-  theme(legend.position = "bottom")
-ggsave(
-  filename=here("output", cohort, "table1", "coverage.png"),
-  plot_coverage,
-  width=15, height=20, units="cm"
-)  
-
-## round to nearest 6 for disclosure control
-threshold <- 6
-
-data_coverage_rounded <-
-  data_coverage %>%
-  group_by(status) %>%
-  mutate(
-    cumuln = roundmid_any(cumuln, to = threshold),
-    n = diff(c(0,cumuln)),
-  )
-
-write_csv(data_coverage_rounded, fs::path(output_dir, "coverage.csv"))
-
 # table 1 style baseline characteristics ----
-
-library('gt')
-library('gtsummary')
 
 var_labels <- list(
   N  ~ "Total N",
+  
   treated ~ "Status",
   age ~ "Age",
   jcvi_ageband ~ "JCVI ageband",
   sex ~ "Sex",
-  #ethnicity_combined ~ "Ethnicity",
+  ethnicity ~ "Ethnicity",
   imd_Q5 ~ "Deprivation",
   region ~ "Region",
+  rural_urban_group ~ "Rurality",
   
-  cev_cv ~ "Clinically vulnerable",
+  bmi ~ "Body mass index",
+  
+  learndis ~ "Learning disability",
+  sev_mental ~ "Severe mental illness",
+  immunosuppressed ~ "Immunouppressed",
+  
+  multimorb ~ "Multimorbidity score",
+  
+  pregnancy ~ "Pregnancy",
+  
+  cv ~ "Clinically vulnerable",
+  cev ~ "Clinically extremely vulnerable",
+  
+  flu_vaccine ~ "Flu vaccine",
   
   vax12_type ~ "Primary course vaccine type",
-  vax2_day ~ "Day of second dose",
+  dayssincevax2 ~ "Days since second dose",
   
-  #prior_tests_cat ~ "Number of SARS-CoV-2 tests",
-  prior_covid_infection ~ "Prior documented SARS-CoV-2 infection"
+  prior_test_cat ~ "Number of SARS-CoV-2 tests during unvaccinated period",
+  prior_covid_infection ~ "Prior documented SARS-CoV-2 infection",
+  time_since_infection ~ "Time since last evidence of SARS-CoV-2 infection"
+  
+  
+  
 ) %>%
 set_names(., map_chr(., all.vars))
 
@@ -187,46 +137,5 @@ raw_stats_redacted <- raw_stats %>%
 
 write_csv(raw_stats_redacted, fs::path(output_dir, "table1.csv"))
 
-
-
-# flowchart ----
-
-# data_flowchart_match <-
-#   read_rds(here("output", "data", "data_inclusioncriteria.rds")) %>%
-#   left_join(
-#     data_matchstatus %>% select(patient_id, matched),
-#     by="patient_id"
-#   ) %>%
-#   mutate(
-#     c7 = c6 & matched,
-#   ) %>%
-#   select(-patient_id, -matched) %>%
-#   group_by(vax3_type) %>%
-#   summarise(
-#     across(.fns=sum)
-#   ) %>%
-#   pivot_longer(
-#     cols=-vax3_type,
-#     names_to="criteria",
-#     values_to="n"
-#   ) %>%
-#   group_by(vax3_type) %>%
-#   mutate(
-#     n_exclude = lag(n) - n,
-#     pct_exclude = n_exclude/lag(n),
-#     pct_all = n / first(n),
-#     pct_step = n / lag(n),
-#     crit = str_extract(criteria, "^c\\d+"),
-#     criteria = fct_case_when(
-#       crit == "c0" ~ "Aged 18+ and received booster dose of BNT162b2 or mRNA-1273 between 29 October 2021 and 31 January 2022", # paste0("Aged 18+\n with 2 doses on or before ", format(study_dates$lastvax2_date, "%d %b %Y")),
-#       crit == "c1" ~ "  with no missing demographic information",
-#       crit == "c2" ~ "  with homologous primary vaccination course of BNT162b2 or ChAdOx1",
-#       crit == "c3" ~ "  and not a health and social care worker",
-#       crit == "c4" ~ "  and not a care/nursing home resident, end-of-life or housebound",
-#       crit == "c5" ~ "  and no COVID-19-related events within 90 days",
-#       crit == "c6" ~ "  and not admitted in hospital at time of booster",
-#       crit == "c7" ~ "  and successfully matched",
-#       TRUE ~ NA_character_
-#     )
-#   )
-
+# cross tab cv and cev ----
+table(data_matched$cev_cv, data_matched$cev)

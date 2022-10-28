a17149f22a9a81c0791662ab70038e5d8e03092e
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-10-28 18:38:48 +0100

    Update coverage.R

--- analysis/matching/coverage.R
+++ analysis/matching/coverage.R
@@ -27,11 +27,9 @@ args <- commandArgs(trailingOnly=TRUE)
 
 if(length(args)==0){
   # use for interactive testing
-  removeobjects <- FALSE
   cohort <- "mrna"
 } else {
   #FIXME replace with actual eventual action variables
-  removeobjects <- TRUE
   cohort <- args[[1]]
 }
 
@@ -40,84 +38,201 @@ if(length(args)==0){
 dates <- study_dates[[cohort]]
 
 
-## create output directories ----
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("")) {
+  
+  ## Import released data ----
+  release_dir <- ""
+  
+  output_dir <- here("output", release_dir, "figures")
+  fs::dir_create(output_dir)
+  
+  data_coverage_rounded <- read_csv(fs::path(release_dir, "data_coverage.csv"))
+  
+  
+} else {
+  
+  ## create output directories ----
+  
+  output_dir <- here("output", cohort, "match", "coverage")
+  fs::dir_create(output_dir)
+  
+  ## Import data and derive some variables ----
+  data_matched <- read_rds(ghere("output", cohort, "match", "data_matched.rds")) 
+  
+  data_treatedeligible_matchstatus <- read_rds(here("output", cohort, "match", "data_treatedeligible_matchstatus.rds"))
+  
+  # matching coverage for boosted people
+  data_coverage <-
+    data_treatedeligible_matchstatus %>%
+    mutate(eligible=1) %>%
+    group_by(vax3_type, vax3_date) %>%
+    summarise(
+      n_eligible = sum(eligible, na.rm=TRUE),
+      n_matched = sum(matched, na.rm=TRUE),
+      .groups = "keep"
+    ) %>%
+    ungroup() %>%
+    mutate(
+      n_unmatched = n_eligible - n_matched,
+    ) %>%
+    pivot_longer(
+      cols = c(n_unmatched, n_matched),
+      names_to = "status",
+      names_prefix = "n_",
+      values_to = "n"
+    ) %>%
+    arrange(vax3_type, vax3_date, status) %>%
+    group_by(vax3_type, vax3_date, status) %>%
+    summarise(
+      n = sum(n),
+      .groups = "keep"
+    ) %>%
+    ungroup(vax3_date) %>%
+    complete(
+      vax3_date = full_seq(.$vax3_date, 1), # go X days before to
+      fill = list(n=0)
+    ) %>%
+    mutate(
+      cumuln = cumsum(n)
+    ) %>%
+    ungroup() %>%
+    mutate(
+      status = factor(status, levels=c("unmatched", "matched")),
+    ) %>%
+    arrange(vax3_type, status, vax3_date) 
+  
+  # save for release
+  data_coverage_rounded <-
+    data_coverage %>%
+    group_by(vax3_type, status) %>%
+    mutate(
+      cumuln = roundmid_any(cumuln, to = threshold),
+      n = diff(c(0,cumuln)),
+    ) %>%
+    ungroup()
+  
+  write_csv(data_coverage_rounded, fs::path(output_dir, "data_coverage.csv"))
+  
+}
 
-output_dir <- here("output", cohort, "table1")
-fs::dir_create(output_dir)
+## plot matching coverage ----
 
-## Import data and derive some variables ----
-
-data_matched <- read_rds(ghere("output", cohort, "match", "data_matched.rds")) 
-
-data_treatedeligible_matchstatus <- read_rds(here("output", cohort, "match", "data_treatedeligible_matchstatus.rds"))
-
-
-# matching coverage on each day of recruitment period ----
-
-
-# matching coverage for boosted people
-data_coverage <-
-  data_treatedeligible_matchstatus %>%
-  group_by(vax3_type, vax3_date) %>%
-  summarise(
-    n_eligible = n(),
-    n_matched = sum(matched, na.rm=TRUE),
-    .groups = "keep"
-  ) %>%
-  ungroup() %>%
+data_plot <- data_coverage_rounded %>%
   mutate(
-    n_unmatched = n_eligible - n_matched,
-  ) %>%
-  pivot_longer(
-    cols = c(n_unmatched, n_matched),
-    names_to = "status",
-    names_prefix = "n_",
-    values_to = "n"
-  ) %>%
-  arrange(vax3_type, vax3_date, status) %>%
-  group_by(vax3_type, vax3_date, status) %>%
-  summarise(
-    n = sum(n),
-    .groups = "keep"
-  ) %>%
-  group_by(vax3_type, status) %>%
-  complete(
-    vax3_date = full_seq(c(dates$start_date, dates$end_date), 1), # go X days before to
-    fill = list(n=0)
-  ) %>%
-  mutate(
-    cumuln = cumsum(n)
-  ) %>%
-  ungroup() %>%
-  mutate(
-    status = factor(status, levels=c("unmatched", "matched")),
-    status_descr = fct_recoderelevel(status, recoder$status)
-  ) %>%
-  arrange(status_descr, vax3_date)
-
-# plot coverage for checking (not release)
-plot_coverage <- data_coverage %>%
-  pivot_longer(cols = c(n, cumuln)) %>%
-  mutate(across(name, factor, levels = c("n", "cumuln"))) %>%
-  ggplot(aes(x=vax3_date, y=value, fill=status_descr)) +
-  geom_bar(stat="identity") +
-  facet_grid(name~vax3_type, scales = "free_y") +
-  theme(legend.position = "bottom")
-ggsave(
-  filename=here("output", cohort, "table1", "coverage.png"),
-  plot_coverage,
-  width=15, height=20, units="cm"
-)  
-
-## round to nearest 6 for disclosure control
-threshold <- 6
-
-data_coverage_rounded <-
-  data_coverage %>%
-  group_by(status) %>%
-  mutate(
-    cumuln = roundmid_any(cumuln, to = threshold),
-    n = diff(c(0,cumuln)),
+    vax3_type_indicator = vax3_type=="pfizer",
+    treatment_descr = fct_recoderelevel(as.character(vax3_type), recoder$vax3_type),
+    status_descr = fct_recoderelevel(status, recoder$status),
+    n=n*((vax3_type_indicator*2) - 1),
+    cumuln=cumuln*((vax3_type_indicator*2) - 1)
   )
 
-write_csv(data_coverage_rounded, fs::path(output_dir, "coverage.csv"))
+xmin <- min(data_plot$vax3_date )
+xmax <- max(data_plot$vax3_date )+1
+
+# this is necessary because there is an older version of a package in opensafely and I think it requires breaks to be unique
+if(Sys.getenv("OPENSAFELY_BACKEND") %in% c("")) {
+  y_labels <- ~scales::label_number(accuracy = 1, big.mark=",")(abs(.x))
+} else {
+  y_labels <- waiver()
+}
+
+plot_coverage_n <-
+  data_plot %>%
+  ggplot() +
+  geom_col(
+    aes(
+      x=vax3_date+0.5,
+      y=n,
+      group=paste0(vax3_type,status),
+      fill=treatment_descr,
+      alpha=fct_rev(status),
+      colour=NULL
+    ),
+    position=position_stack(reverse=TRUE),
+    #alpha=0.8,
+    width=1
+  ) +
+  geom_hline(yintercept = 0, colour="black") +
+  scale_x_date(
+    breaks = unique(lubridate::ceiling_date(data_coverage$vax3_date, "1 month")),
+    limits = c(xmin-1, NA),
+    labels = scales::label_date("%d/%m"),
+    expand = expansion(add=1),
+  ) +
+  scale_y_continuous(
+    labels = y_labels,
+    expand = expansion(c(0, NA)),
+  ) +
+  scale_fill_brewer(type="qual", palette="Set2") +
+  scale_colour_brewer(type="qual", palette="Set2") +
+  scale_alpha_discrete(range= c(0.8,0.4))+
+  labs(
+    x="Date",
+    y="Booster vaccines per day",
+    colour=NULL,
+    fill=NULL,
+    alpha=NULL
+  ) +
+  theme_minimal() +
+  theme(
+    axis.line.x.bottom = element_line(),
+    axis.text.x.top=element_text(hjust=0),
+    strip.text.y.right = element_text(angle = 0),
+    axis.ticks.x=element_line(),
+    legend.position = "bottom"
+  ) +
+  NULL
+
+plot_coverage_n
+
+ggsave(plot_coverage_n, filename="coverage_count.png", path=output_dir)
+
+plot_coverage_cumuln <-
+  data_plot %>%
+  ggplot()+
+  geom_col(
+    aes(
+      x=vax3_date+0.5,
+      y=cumuln,
+      group=paste0(vax3_type,status),
+      fill=treatment_descr,
+      alpha=fct_rev(status),
+      colour=NULL
+    ),
+    position=position_stack(reverse=TRUE),
+    width=1
+  )+
+  geom_rect(xmin=xmin, xmax= xmax+1, ymin=-6, ymax=6, fill="grey", colour="transparent")+
+  scale_x_date(
+    breaks = unique(lubridate::ceiling_date(data_coverage$vax3_date, "1 month")),
+    limits = c(xmin-1, NA),
+    labels = scales::label_date("%d/%m"),
+    expand = expansion(add=1),
+  )+
+  scale_y_continuous(
+    labels = y_labels,
+    expand = expansion(c(0, NA))
+  )+
+  scale_fill_brewer(type="qual", palette="Set2")+
+  scale_colour_brewer(type="qual", palette="Set2")+
+  scale_alpha_discrete(range= c(0.8,0.4))+
+  labs(
+    x="Date",
+    y="Cumulative booster vaccines",
+    colour=NULL,
+    fill=NULL,
+    alpha=NULL
+  ) +
+  theme_minimal()+
+  theme(
+    axis.line.x.bottom = element_line(),
+    axis.text.x.top=element_text(hjust=0),
+    strip.text.y.right = element_text(angle = 0),
+    axis.ticks.x=element_line(),
+    legend.position = "bottom"
+  )+
+  NULL
+
+plot_coverage_cumuln
+
+ggsave(plot_coverage_cumuln, filename="coverage_stack.png", path=output_dir)

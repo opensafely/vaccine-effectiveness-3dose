4f30f9eb224bc2714378338553fb3b93b76677a4
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-12-16 16:21:34 +0000

    Update cox.R

--- analysis/model/cox.R
+++ analysis/model/cox.R
@@ -40,10 +40,10 @@ args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   cohort <- "mrna"
-  type <- "adj"
-  subgroup <- "prior_covid_infection"
+  type <- "unadj"
+  subgroup <- "agegroup"
   variant_option <- "ignore" # ignore, split, restrict (delta, transition, omicron)
-  outcome <- "postest"
+  outcome <- "covidadmitted"
   
 } else {
   cohort <- args[[1]]
@@ -166,9 +166,6 @@ coxcontrast <- function(data, adj = FALSE, cuts=NULL){
     ) 
   
   cox_formula_string <- "Surv(tstart, tstop, ind_outcome) ~ treated"
-  if (n_distinct(data_split$period_id) > 1) {
-    cox_formula_string <- str_c(cox_formula_string, ":strata(period_id)")
-  }
   
   # only keep periods with >2 events per level of exposure
   data_cox_nested <- data_split %>%
@@ -181,9 +178,15 @@ coxcontrast <- function(data, adj = FALSE, cuts=NULL){
     select(-n_events, -min_events) %>%
     group_by(!!subgroup_sym) %>%
     nest() %>%
-    mutate(
-      cox_formula = cox_formula_string
-    )
+    # add strata(period_id) to cox_formula if period_id has more than one distinct values
+    mutate(cox_formula = map(data, ~{
+      if_else(
+        n_distinct(.x$period_id) == 1,
+        cox_formula_string,
+        str_c(cox_formula_string, ":strata(period_id)")
+      )
+    })) %>%
+    unnest(cox_formula)
   
   
   if (nrow(data_cox_nested) == 0) {

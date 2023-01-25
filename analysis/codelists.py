96b27fe8d79113672ad7e7169a1d1855373ab365
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-01-25 07:15:50 +0000

    add fracture and cvd variables

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -83,130 +83,6 @@ ethnicity_16 = codelist_from_csv(
     category_column="Grouping_16",
 )
 
-# 
-# solid_organ_transplantation = codelist_from_csv(
-#     "codelists/opensafely-solid-organ-transplantation.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# 
-# lung_cancer = codelist_from_csv(
-#     "codelists/opensafely-lung-cancer.csv", system="ctv3", column="CTV3ID",
-# )
-# haematological_cancer = codelist_from_csv(
-#     "codelists/opensafely-haematological-cancer.csv", system="ctv3", column="CTV3ID",
-# )
-# bone_marrow_transplant = codelist_from_csv(
-#     "codelists/opensafely-bone-marrow-transplant.csv", system="ctv3", column="CTV3ID",
-# )
-# cystic_fibrosis = codelist_from_csv(
-#     "codelists/opensafely-cystic-fibrosis.csv", system="ctv3", column="CTV3ID",
-# )
-# 
-# sickle_cell_disease = codelist_from_csv(
-#     "codelists/opensafely-sickle-cell-disease.csv", system="ctv3", column="CTV3ID",
-# )
-# 
-# permanent_immunosuppression = codelist_from_csv(
-#     "codelists/opensafely-permanent-immunosuppression.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# temporary_immunosuppression = codelist_from_csv(
-#     "codelists/opensafely-temporary-immunosuppression.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# chronic_cardiac_disease = codelist_from_csv(
-#     "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID",
-# )
-# learning_disability = codelist_from_csv(
-#     "codelists/opensafely-learning-disabilities.csv",
-#     system="ctv3",
-#     column="CTV3Code",
-# )
-# downs_syndrome = codelist_from_csv(
-#     "codelists/opensafely-down-syndrome.csv",
-#     system="ctv3",
-#     column="code",
-# )
-# cerebral_palsy = codelist_from_csv(
-#     "codelists/opensafely-cerebral-palsy.csv",
-#     system="ctv3",
-#     column="code",
-# )
-# learning_disability_including_downs_syndrome_and_cerebral_palsy = combine_codelists(
-#     learning_disability,
-#     downs_syndrome,
-#     cerebral_palsy,
-# )
-# dialysis = codelist_from_csv(
-#     "codelists/opensafely-dialysis.csv", system="ctv3", column="CTV3ID",
-# )
-# other_respiratory_conditions = codelist_from_csv(
-#     "codelists/opensafely-other-respiratory-conditions.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# heart_failure = codelist_from_csv(
-#     "codelists/opensafely-heart-failure.csv", system="ctv3", column="CTV3ID",
-# )
-# other_heart_disease = codelist_from_csv(
-#     "codelists/opensafely-other-heart-disease.csv", system="ctv3", column="CTV3ID",
-# )
-# 
-# chronic_cardiac_disease = codelist_from_csv(
-#     "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID",
-# )
-# 
-# chemotherapy_or_radiotherapy = codelist_from_csv(
-#     "codelists/opensafely-chemotherapy-or-radiotherapy.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# cancer_excluding_lung_and_haematological = codelist_from_csv(
-#     "codelists/opensafely-cancer-excluding-lung-and-haematological.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# 
-# current_copd = codelist_from_csv(
-#     "codelists/opensafely-current-copd.csv", system="ctv3", column="CTV3ID"
-# )
-# 
-# dementia = codelist_from_csv(
-#     "codelists/opensafely-dementia-complete.csv", system="ctv3", column="code"
-# )
-# 
-# diabetes = codelist_from_csv(
-#     "codelists/opensafely-diabetes.csv", system="ctv3", column="CTV3ID"
-# )
-# 
-# dmards = codelist_from_csv(
-#     "codelists/opensafely-dmards.csv", system="snomed", column="snomed_id",
-# )
-# 
-# 
-# chronic_liver_disease = codelist_from_csv(
-#     "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID",
-# )
-# other_neuro = codelist_from_csv(
-#     "codelists/opensafely-other-neurological-conditions.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# 
-# psychosis_schizophrenia_bipolar_affective_disease = codelist_from_csv(
-#     "codelists/opensafely-psychosis-schizophrenia-bipolar-affective-disease.csv",
-#     system="ctv3",
-#     column="CTV3Code",
-# )
-# 
-# asplenia = codelist_from_csv(
-#     "codelists/opensafely-asplenia.csv", system="ctv3", column="CTV3ID"
-# )
-
-
 ## PRIMIS
 
 
@@ -508,3 +384,126 @@ fractures_snomedECDS = codelist(
   system="snomed"
 )
 
+# for defining deaths due to cardiovascvular disease
+
+# Acute myocardial infarction
+ami_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-ami_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Ischaemic stroke
+stroke_isch_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_isch_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Deep vein thrombosis
+dvt_dvt_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-dvt_dvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Deep vein thrombosis in pregnancy
+dvt_pregnancy_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dvt_pregnancy_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Pulmonary embolism
+pe_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-pe_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Transient ischaemic attack
+tia_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-tia_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Subarachnoid haemorrhage and haemorrhagic stroke
+stroke_sah_hs_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_sah_hs_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Ischaemic stroke
+stroke_isch_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-stroke_isch_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Heart failure
+hf_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-hf_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Angina
+angina_icd10 = codelist_from_csv(
+    "codelists/user-RochelleKnight-angina_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Other arterial embolism
+other_arterial_embolism_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-other_arterial_embolism_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Portal vein thrombosis
+portal_vein_thrombosis_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-portal_vein_thrombosis_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Cerebral vein thrombosis
+dvt_icvt_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-dvt_icvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Other deep vein thrombosis
+other_dvt_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-other_dvt_icd10.csv",
+    system="icd10",
+    column="code",
+)
+# Cerebral vein thrombosis in pregnancy
+icvt_pregnancy_icd10 = codelist_from_csv(
+    "codelists/user-elsie_horne-icvt_pregnancy_icd10.csv",
+    system="icd10",
+    column="code",
+)
+
+cvd_combined = combine_codelists(
+    # Acute myocardial infarction
+    ami_icd10,
+    # Ischaemic stroke
+    stroke_isch_icd10,
+    # Deep vein thrombosis (all_dvt_codes_icd10)
+    dvt_dvt_icd10,
+    dvt_pregnancy_icd10,
+    # Pulmonary embolism
+    pe_icd10,
+    # Transient ischaemic attack
+    tia_icd10,
+    # Subarachnoid haemorrhage and haemorrhagic stroke
+    stroke_sah_hs_icd10,
+    # Heart failure
+    hf_icd10,
+    # Angina
+    angina_icd10,
+    # Other arterial embolism
+    other_arterial_embolism_icd10,
+    # Venous thromboembolism events (all_vte_codes_icd10)
+    # Portal vein thrombosis
+    portal_vein_thrombosis_icd10, 
+    # Cerebral vein thrombosis
+    dvt_icvt_icd10, 
+    # Other deep vein thrombosis
+    other_dvt_icd10, 
+    # Cerebral vein thrombosis in pregnancy
+    icvt_pregnancy_icd10, 
+)
